b51b82155e10b0728468ffc4fcc98eac2ee5ff43
saifulalam <saifulalamsaimon85@gmail.com>
2025-12-13 21:34:54 +0600

    big fix

--- event_management/settings.py
+++ event_management/settings.py
@@ -74,6 +74,7 @@ DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
         default='postgresql://event_management_db_hf12_user:vwECmexGhtsQB9af3uyQmWxhDhAJBeCr@dpg-d4une463jp1c73e6af10-a.oregon-postgres.render.com/event_management_db_hf12',
+        
         conn_max_age=600
     )
 }
