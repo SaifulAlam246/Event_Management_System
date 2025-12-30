bcc96e16aba8ae555019877fdd6856374d5b7c4b
saifulalam <saifulalamsaimon85@gmail.com>
2025-12-30 21:31:31 +0600

    database port problem solved

--- event_management/settings.py
+++ event_management/settings.py
@@ -86,16 +86,16 @@ DATABASES = {
 }
 
 # for postgres sql
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Event_Management',
-        'USER': 'postgres',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'Event_Management',
+#         'USER': 'postgres',
+#         'PASSWORD': 'password',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
